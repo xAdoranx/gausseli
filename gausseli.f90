eff6d88dffa5b9cc1ae8f8e21fd30495a21a66d2
Adrian Messow <adrian-messow@t-online.de>
2017-05-16 11:21:19 +0200

    LR-cutting implemented

--- gausseli.f90
+++ gausseli.f90
@@ -4,75 +4,106 @@ program gausseli
   integer :: ii, jj, dim, nmax
   integer, allocatable :: num(:)
   integer, parameter :: dp = selected_real_kind(12,99)
-  real(dp), allocatable :: array(:,:), arrayb(:,:), res(:), bvec(:), tmp(:,:)
+  real(dp), allocatable :: onemat(:,:), Rarray(:,:), Larray(:,:), Parray(:,:), preres(:), res(:)&
+      &, bvec(:), tmp(:,:)
+  
   character(len=26) :: outputform = '(1000000F10.2)'
 
   open(21, file="gauss.inp", status="old", form="formatted", action="read")
   open(11, file="output.dat", status="replace", form="formatted", action="write")
   
   mainloop: do
-   ! write(*,*) "To stop make Matrix Dimension negative or 0"
-   ! write(*,*) "Please enter the Matrix Dimension of A in A*x=b"
+    
     read(21,*) dim  
     if (dim <= 0) then
       exit mainloop
     end if
     
-    allocate(array(dim,dim))
+    allocate(Rarray(dim,dim))
     allocate(bvec(dim))
-    !write(*,*) "Please enter the Matrix values row by row:"
-    read(21,*) array
-    !write(*,*) "Please enter the values of the solution vector b:"
+    
+    read(21,*) Rarray
+    
     read(21,*) bvec
-    array = transpose(array)
+    Rarray = transpose(Rarray)
 
     write(*,*) "Input Matrix:"
     do ii = 1, dim
-      write(*,outputform) array(ii,:)
+      write(*,outputform) Rarray(ii,:)
     end do
 
-    allocate(arrayb(dim,dim+1))
-    arrayb(:,1:dim)=array
-    arrayb(:,dim+1)=bvec
+    allocate(onemat(dim,dim))
+    allocate(Larray(dim,dim))
+    allocate(Parray(dim,dim))
+    onemat = 0
+    do ii=1, dim
+      onemat(ii,ii)=1
+    end do
+    Larray = onemat
+    Parray = onemat
 
-    allocate(tmp(1,dim+1))
+    allocate(tmp(1,dim))
     gauss: do ii = 1, dim
-      write(12,*) ii
-      tmp(1,:) = arrayb(ii,:)
-      nmax = ii - 1 + maxloc(abs(arrayb(ii:dim,ii)),dim=1) !pivot
+
+      nmax = ii - 1 + maxloc(abs(Rarray(ii:dim,ii)),dim=1) !pivot
+
+      tmp(1,:) = Rarray(ii,:)
+      Rarray(ii,:) = Rarray(nmax,:)
+      Rarray(nmax,:) = tmp(1,:)
       
-      arrayb(ii,:) = arrayb(nmax,:)
-      arrayb(nmax,:) = tmp(1,:)
+      tmp(1,:) = Parray(ii,:)
+      Parray(ii,:) = Parray(nmax,:)
+      Parray(nmax,:) = tmp(1,:)
 
       do jj = ii + 1, dim
-        arrayb(jj,:) = arrayb(jj,:)-(arrayb(ii,:) * (arrayb(jj,ii)/arrayb(ii,ii)))
+        tmp(1,1)= Rarray(jj,ii)/Rarray(ii,ii)
+        
+        Rarray(jj,:) = Rarray(jj,:)-(Rarray(ii,:) * tmp(1,1))
+        Larray(jj,:) = Larray(jj,:)-(Larray(ii,:) * tmp(1,1))       
+        
       end do
       
-      if (abs(arrayb(ii,ii)) < 1E-12_dp) then
+      if (abs(Rarray(ii,ii)) < 1E-12_dp) then
         write(*,*) "underterminated System"
-        exit mainloop
+        cycle mainloop
       end if  
     end do gauss
 
     write(11,*) "Upper triangle matrix:"
     do ii = 1,dim
-      write(11,outputform) arrayb(ii,:)
+      write(11,outputform) Rarray(ii,:)
     end do
+    write(11,*) "Lower triangle matrix:"
+    do ii = 1,dim
+      write(11,outputform) Larray(ii,:)
+    end do
+    
    
     allocate(res(dim))
-    res(dim) = arrayb(dim,dim+1) / arrayb(dim,dim)
-    fin: do ii = dim-1, 1, -1
-      res(ii) = (arrayb(ii, dim + 1) - dot_product(arrayb(ii,ii+1:dim),res(ii:dim))) / arrayb(ii,ii)
-    end do fin
+    allocate(preres(dim))
+    bvec = matmul(Parray, bvec)
+    preres(1) = bvec(1)
+    do ii = 2, dim
+      preres(ii) = bvec(ii) - dot_product(Larray(ii,1:ii-1),preres(1:ii-1))
+    end do
+    res(dim) = preres(dim)/Rarray(dim,dim)
+    do ii = dim-1, 1, -1
+      res(ii) = (preres(ii) - dot_product(Rarray(ii,ii+1:dim),preres(ii+1:dim)))/Rarray(ii,ii)
+      write(*,*) res(ii)
+    end do
+    
 
     output: do ii = 1,dim
       write(11,"(A,I0,A2,F10.6)") "x_", ii, "=", res(ii)
     end do output
 
     deallocate(res)
+    deallocate(preres)
+    deallocate(onemat)
     deallocate(tmp)
-    deallocate(array)
-    deallocate(arrayb)
+    deallocate(Rarray)
+    deallocate(Larray)
+    deallocate(Parray)
     deallocate(bvec)
         
   end do mainloop
